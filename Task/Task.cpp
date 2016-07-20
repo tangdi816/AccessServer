081ab63a028b26dd231772c591da88b2bf59abd6
tangdi <tangdi@gamil.com>
2016-07-20 22:50:17 +0800

    超级糖果 接入服务器建库

---
+++ Task/Task.cpp
@@ -0,0 +1,10 @@
+#include "Task.h"
+
+
+
+CTask::CTask()
+{}
+
+
+CTask::~CTask()
+{}
---
+++ Task/Task.h
@@ -0,0 +1,12 @@
+#ifndef __TASK_H__
+#define __TASK_H__
+
+class CTask
+{
+public:
+  CTask();
+  ~CTask();
+};
+#endif // 
+
+
